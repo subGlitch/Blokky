5b9d3a340ec74b0f39a925b5f2d00ce54b916bd3
subGlitch <d.sankov@gmail.com>
2020-09-16 21:18:57 +0300

    Spawner

--- Assets/Scripts/PrefabEntities.cs
+++ Assets/Scripts/PrefabEntities.cs
@@ -16,13 +16,7 @@ public class PrefabEntities : MonoBehaviour, IDeclareReferencedPrefabs, IConvert
 
 
 	public void Convert( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
-	{
+	=>
 		prefab_Block		= conversionSystem.GetPrimaryEntity( goPrefab_Block );
-
-		/*
-        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
-		entityManager.SetName( prefab_Block, "prefab_Block" );
-		*/
-	}
 }
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 
@@ -6,22 +9,21 @@ public class Spawner : MonoBehaviour
 {
 	void Start()
 	{
-		/*
-        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
-		EntityArchetype entityArchetype		= entityManager.CreateArchetype(
-												typeof( GridPositionComponent ),
-												typeof( IsTakenComponent )
-		);
-		var nativeArray						= entityManager.CreateEntity( entityArchetype, 3, Allocator.Temp );
-	
-		nativeArray.Dispose();		// Not necessary...
-
-		// entityManager.SetComponentData( entity, new GridPositionComponent{ position = new int2( 3, 5 ) } );
-		*/
-
+		Vector2Int size			= new Vector2Int( 17, 17 );
+		int count				= size.x * size.y;
 
         EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
-		Entity entity						= entityManager.Instantiate( PrefabEntities.prefab_Block );
+		var nativeArray						= entityManager.Instantiate( PrefabEntities.prefab_Block, count, Allocator.Temp );
+
+		for (int y = 0; y < size.y; y ++)
+		for (int x = 0; x < size.x; x ++)
+		{
+			Entity entity		= nativeArray[ y * size.x + x ];
+
+			entityManager.SetComponentData( entity, new Translation { Value = new float3( x, y, 0 ) } );
+		}
+
+		nativeArray.Dispose();
 	}
 }
 
