e0adab057adc95cfc3647d993e3d3fcb70faa8e1
subGlitch <d.sankov@gmail.com>
2020-09-16 21:57:07 +0300

    Spawner

---
+++ Assets/Scripts/GridPositionComponent.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+
+public struct GridPositionComponent : IComponentData
+{
+	public int x;
+	public int y;
+
+
+	public GridPositionComponent( int x, int y )
+	{
+		this.x		= x;
+		this.y		= y;
+	}
+}
+
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,21 +9,42 @@ public class Spawner : MonoBehaviour
 {
 	void Start()
 	{
-		Vector2Int size			= new Vector2Int( 17, 17 );
-		int count				= size.x * size.y;
+		Vector2Int gridSize			= new Vector2Int( 17, 17 );
 
+
+		Vector2 gridWorldSize		= CalcGridWorldSize( gridSize );
+		float blockWidth			= gridWorldSize.x / gridSize.x;
+		float blockScale			= blockWidth;
+
+		int count							= gridSize.x * gridSize.y;
         EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
 		var nativeArray						= entityManager.Instantiate( PrefabEntities.prefab_Block, count, Allocator.Temp );
 
-		for (int y = 0; y < size.y; y ++)
-		for (int x = 0; x < size.x; x ++)
+		for (int y = 0; y < gridSize.y; y ++)
+		for (int x = 0; x < gridSize.x; x ++)
 		{
-			Entity entity		= nativeArray[ y * size.x + x ];
+			Entity entity		= nativeArray[ y * gridSize.x + x ];
 
 			entityManager.SetComponentData( entity, new Translation { Value = new float3( x, y, 0 ) } );
+			entityManager.AddComponentData( entity, new NonUniformScale { Value = new float3( blockScale, blockScale, blockScale ) } );
+			entityManager.AddComponentData( entity, new GridPositionComponent( x, y ) );
 		}
 
 		nativeArray.Dispose();
+	}
+
+
+	Vector2 CalcGridWorldSize( Vector2Int gridSize )
+	{
+		Camera mainCamera			= Camera.main;
+
+		float screenWorldHeight		= mainCamera.orthographicSize * 2;
+		float screenWorldWidth		= screenWorldHeight * mainCamera.aspect;
+
+		float girdWorldWidth		= screenWorldWidth;
+		float girdWorldHeight		= girdWorldWidth * gridSize.y / gridSize.x;
+
+		return new Vector2( girdWorldWidth, girdWorldHeight );
 	}
 }
 
