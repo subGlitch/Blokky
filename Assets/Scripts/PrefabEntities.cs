9400779e7f52dc2e2714938942fe68051ae1d676
subGlitch <d.sankov@gmail.com>
2020-09-16 20:54:52 +0300

    PrefabEntities

---
+++ Assets/Scripts/PrefabEntities.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+
+public class PrefabEntities : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+	public static Entity	prefab_Block;
+
+	public GameObject		goPrefab_Block;
+
+
+	public void DeclareReferencedPrefabs( List< GameObject > referencedPrefabs )
+	=>
+		referencedPrefabs.Add( goPrefab_Block );
+
+
+	public void Convert( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
+	=>
+		prefab_Block		= conversionSystem.GetPrimaryEntity( goPrefab_Block );
+}
+
