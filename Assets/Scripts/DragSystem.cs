a7d0230760f10a7749d331c4485a926ebd96090c
subGlitch <d.sankov@gmail.com>
2020-09-16 22:29:24 +0300

    DragSystem

---
+++ Assets/Scripts/DragSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+public class DragSystem : ComponentSystem
+{
+	protected override void OnUpdate()
+	{
+		if (
+				Input.GetMouseButtonDown( 0 ) ||
+				Input.GetMouseButton( 0 )
+			)
+			Entities.ForEach( (ref Translation translation, ref GridPositionComponent pos) =>
+			{
+				translation.Value		= new float3( (Vector3)(Vector2)Camera.main.ScreenToWorldPoint( Input.mousePosition ) );
+			});
+	}
+}
+
