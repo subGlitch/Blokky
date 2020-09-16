fba9779696a146757b94979ebd1f5b0c36e1c505
subGlitch <d.sankov@gmail.com>
2020-09-16 21:02:56 +0300

    Spawner

--- Assets/Scripts/PrefabEntities.cs
+++ Assets/Scripts/PrefabEntities.cs
@@ -16,7 +16,13 @@ public class PrefabEntities : MonoBehaviour, IDeclareReferencedPrefabs, IConvert
 
 
 	public void Convert( Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem )
-	=>
+	{
 		prefab_Block		= conversionSystem.GetPrimaryEntity( goPrefab_Block );
+
+		/*
+        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
+		entityManager.SetName( prefab_Block, "prefab_Block" );
+		*/
+	}
 }
 
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+	void Start()
+	{
+		/*
+        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
+		EntityArchetype entityArchetype		= entityManager.CreateArchetype(
+												typeof( GridPositionComponent ),
+												typeof( IsTakenComponent )
+		);
+		var nativeArray						= entityManager.CreateEntity( entityArchetype, 3, Allocator.Temp );
+	
+		nativeArray.Dispose();		// Not necessary...
+
+		// entityManager.SetComponentData( entity, new GridPositionComponent{ position = new int2( 3, 5 ) } );
+		*/
+
+
+        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
+		Entity entity						= entityManager.Instantiate( PrefabEntities.prefab_Block );
+	}
+}
+
