835152864827ea3624c5d1a6b8ab4a11c3855638
subGlitch <d.sankov@gmail.com>
2020-09-17 23:23:49 +0300

    Utilities

---
+++ Assets/Scripts/Utilities/MB_Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class MB_Singleton< T > : MonoBehaviour where T : MB_Singleton< T >
+{
+	public static T Instance { get; private set; }
+
+
+	protected virtual void Awake()
+	{
+		Instance		= Utilities.SingletonPattern( (T)this, Instance );
+	}
+
+
+	void OnDestroy()
+	{
+		Instance		= null;
+	}
+}
+
---
+++ Assets/Scripts/Utilities/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+public static class Utilities
+{
+	public static T SingletonPattern< T >( T @this, T instance ) where T : class
+	{
+		if (instance != null)
+			throw new Exception( $"Singleton pattern violation: instance of class { @this.GetType().Name } already exists!" );
+		
+		return @this;
+	}
+}
+
