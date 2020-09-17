149f3d1ea897c6d1b3e12bb5af2d3a25abf2a6f5
subGlitch <d.sankov@gmail.com>
2020-09-17 15:00:39 +0300

    DragSystem

--- Assets/Scripts/DragSystem.cs
+++ Assets/Scripts/DragSystem.cs
@@ -1,29 +1,43 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
 
 public class DragSystem : ComponentSystem
 {
+	Vector2		_mouseLast_w;
+
+
 	protected override void OnUpdate()
 	{
-        EntityManager entityManager			= World.DefaultGameObjectInjectionWorld.EntityManager;
+		bool isDragStart		= Input.GetMouseButtonDown( 0 );
+		bool isDragContinue		= Input.GetMouseButton( 0 );
 
-		if (
-				Input.GetMouseButtonDown( 0 ) ||
-				Input.GetMouseButton( 0 )
-			)
-			Entities
-				.WithAll< DraggableComponent >()
-				.ForEach( (Entity entity) =>
-			{
-				DynamicBuffer< Cell > cells			= entityManager.GetBuffer< Cell >( entity );
-				Translation translation				= new Translation { Value = (Vector3)(Vector2)Camera.main.ScreenToWorldPoint( Input.mousePosition ) };
+		if (!isDragStart && !isDragContinue)
+			return;
+		
+		Vector2 mouse_w			= Camera.main.ScreenToWorldPoint( Input.mousePosition );
 
-				entityManager.SetComponentData( cells[ 0 ].cell, translation );
-			});
+		if (!isDragStart)
+			Shift( mouse_w - _mouseLast_w );
+
+		_mouseLast_w			= mouse_w;
+	}
+
+
+	void Shift( Vector2 shift )
+	{
+	    EntityManager entityManager				= World.DefaultGameObjectInjectionWorld.EntityManager;
+			
+		Entities.ForEach( (Entity entity, ref DraggableComponent draggable) =>
+		{
+			DynamicBuffer< Cell > cells			= entityManager.GetBuffer< Cell >( entity );
+			Translation cellTranslation			= entityManager.GetComponentData< Translation >( cells[ 0 ].cell );
+			cellTranslation.Value				+= (float3)(Vector3)shift;
+
+			entityManager.SetComponentData( cells[ 0 ].cell, cellTranslation );
+		});
 	}
 }
 
--- Assets/Scripts/DraggableComponent.cs
+++ Assets/Scripts/DraggableComponent.cs
@@ -3,6 +3,5 @@
 
 public struct DraggableComponent : IComponentData
 {
-	
 }
 
