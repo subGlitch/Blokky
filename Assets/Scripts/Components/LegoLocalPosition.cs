0ff91a41ebfc0f8383f2f3a89926da38f9f51f71
subGlitch <d.sankov@gmail.com>
2020-09-18 12:14:31 +0300

    refact

--- Assets/Scripts/Components/LegoLocalPosition.cs
+++ Assets/Scripts/Components/LegoLocalPosition.cs
@@ -4,9 +4,9 @@ using Unity.Mathematics;
 
 public struct LegoLocalPosition : IComponentData
 {
-	public int2		position;
+	public int2		Value;
 
 
-	public LegoLocalPosition( int2 position )		=> this.position		= position;
+	public LegoLocalPosition( int2 value )		=> Value		= value;
 }
 
