d8e0ac4f168511cc05d4bfa64c63d1a2736ca1f2
subGlitch <d.sankov@gmail.com>
2020-09-19 13:21:31 +0300

    block scales

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,26 +3,16 @@
 
 public static class Grid
 {
-	public static float		LegoScale;
-	public static float		LegoSize				=> LegoPrefabSize * LegoScale;
-
-
-	static float			LegoPrefabSize			=> 1;		// Lego prefab has size of 1 unit
-	
-
-	public static void SetGridSize( Vector2Int gridSize, RectTransform space )
-	{
-		LegoScale		= CalcScale( gridSize, space );
-	}
-
-
 	public static float CalcScale( Vector2Int gridSize, RectTransform space )
 	{
 		Rect availableSpace_w		= space.GetWorldRect();
 		Rect rect_w					= CalcGridWorldRect( gridSize, availableSpace_w );
 		float legoSize_w			= rect_w.size.x / gridSize.x;
 
-		return legoSize_w / LegoPrefabSize;
+		float legoPrefabSize		= 1;
+		float scale					= legoSize_w / legoPrefabSize;
+
+		return scale;
 	}
 
 
--- Assets/Scripts/UI/BlokkyEditor.cs
+++ Assets/Scripts/UI/BlokkyEditor.cs
@@ -3,9 +3,6 @@
 
 public class BlokkyEditor : MB_Singleton< BlokkyEditor >
 {
-	public static float UiScale;
-
-
 #pragma warning disable 0649
 
 	[SerializeField] RectTransform	_spaceForGrid;
@@ -16,18 +13,18 @@ public class BlokkyEditor : MB_Singleton< BlokkyEditor >
 
 	public void Init( Vector2Int gridSize )
 	{
-		// Init Grid
-		Grid.SetGridSize( gridSize, _spaceForGrid );
-		Factory.Instance.CreateBlock( _spaceForGrid.position, gridSize, Grid.LegoScale, false );
+		// Calc scales
+		float gridScale			= Grid.CalcScale( gridSize, _spaceForGrid );
+		float uiScale			= Grid.CalcScale( new Vector2Int( 5, 5 ), _dragStartArea );
 
-		// Calc UI scale
-		UiScale			= Grid.CalcScale( new Vector2Int( 5, 5 ), _dragStartArea );
+		// Init Grid
+		Factory.Instance.CreateBlock( _spaceForGrid.position, gridSize, gridScale, false );
 
 		// UI selected block
-		Factory.Instance.CreateBlock( DragStartArea.Instance.gameObject.transform.position, new Vector2Int( 3, 2 ), UiScale, false );
+		Factory.Instance.CreateBlock( DragStartArea.Instance.gameObject.transform.position, new Vector2Int( 3, 2 ), uiScale, false );
 
 		// Test
-		Factory.Instance.CreateBlock( new Vector2( 0, -4 ), new Vector2Int( 3, 2 ), UiScale, true );
+		Factory.Instance.CreateBlock( new Vector2( 0, -4 ), new Vector2Int( 3, 2 ), uiScale, true );
 	}
 }
 
