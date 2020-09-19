d952fae640f6ca34e3d8081a35c73f9b4aaf203f
subGlitch <d.sankov@gmail.com>
2020-09-19 18:54:25 +0300

    UI

---
+++ Assets/Scripts/Utilities/Touchable.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.UI;
+
+
+// http://answers.unity.com/answers/851816/view.html
+
+ 
+public class Touchable : Text
+{
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+}
+
+
+/*
+// http://answers.unity.com/answers/1165070/view.html
+
+ 
+namespace UnityEngine.UI
+{
+	public class Touchable : Graphic
+	{
+		public override bool Raycast( Vector2 sp, Camera eventCamera )
+		{
+			// return base.Raycast( sp, eventCamera );
+			return true;
+		}
+
+ 
+		protected override void OnPopulateMesh( VertexHelper vh )
+		{
+			vh.Clear();
+		}
+	}
+}
+*/
+
