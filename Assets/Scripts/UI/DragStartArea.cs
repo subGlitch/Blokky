baa0b69d42eeaaa2aeddeed4df5427bf696f6e65
subGlitch <d.sankov@gmail.com>
2020-09-19 13:56:08 +0300

    refact

--- Assets/Scripts/UI/DragStartArea.cs
+++ Assets/Scripts/UI/DragStartArea.cs
@@ -4,25 +4,24 @@ using UnityEngine.EventSystems;
 
 public class DragStartArea : MB_Singleton< DragStartArea >, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-	public void OnPointerDown(PointerEventData eventData)
-	{
+	public void OnPointerDown( PointerEventData eventData )
+	=>
 		Factory.Instance.CreateBlock(
 			transform.position,
 			new Vector2Int( 3, 2 ),
 			Flags.IsDraggable
 		);
+
+
+	public void OnDrag( PointerEventData eventData )
+	{
+		// Do nothing. Everything is handled by ECS systems.
 	}
 
 
-	public void OnDrag(PointerEventData eventData)
+	public void OnPointerUp( PointerEventData eventData )
 	{
-		Utilities.Log();
-	}
-
-
-	public void OnPointerUp(PointerEventData eventData)
-	{
-		Utilities.Log();
+		
 	}
 }
 
